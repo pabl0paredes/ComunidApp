7a52bd462a19e76b3ffd88dc24627886f6a1d845
Pablo Paredes Haz <pparedeshaz@gmail.com>
2025-11-15 15:34:31 -0300

    controladores y vistas vacías

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,25 @@
+class ChatsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def hidden
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,40 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :communities do
+    resources :chats, only: [:index, :new, :create]
+    resources :common_expenses, only: [:new, :create, :index]
+    resources :common_spaces, only: [:new, :create, :index]
+    resources :neighbors, only: [:index, :new, :create]
+    resources :administrators, only: [:show]
+  end
+
+  resources :administrators, only: [:new, :create, :destroy]
+  resources :neighbors, only: [:show, :edit, :update, :destroy]
+
+  resources :common_spaces, only: [:show, :destroy, :edit, :update] do
+    resources :bookings, only: [:create]
+    resources :usable_hours, only: [:create, :update]
+  end
+
+  resources :usable_hours, only: [:destroy]
+
+  resources :bookings, only: [:show, :destroy, :edit, :update, :index]
+
+  resources :common_expenses, only: [:show, :destroy, :edit, :update] do
+    resources :expense_details, only: [:new, :create, :index]
+  end
+
+  resources :expense_details, only: [:show, :destroy, :edit, :update]
+
+  resources :chats, only: [:show, :destroy, :edit, :update] do
+    collection do
+      get :hidden
+    end
+    resources :messages, only: [:create]
+  end
+
+  resources :show_chats, only: [:create, :udpate]
+
 end
