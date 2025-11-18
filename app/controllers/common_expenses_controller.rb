34b55edef48662b69ac93b69fe3f2c269ee1514f
gonzalo aime <aimegonzalo@gmail.com>
2025-11-18 23:09:41 +0000

    expenses controller y details controller + vistas básicas

--- app/controllers/common_expenses_controller.rb
+++ app/controllers/common_expenses_controller.rb
@@ -1,22 +1,59 @@
 class CommonExpensesController < ApplicationController
+  before_action :set_community, only: [:index, :new, :create]
+  before_action :set_common_expense, only: [:show, :edit, :update, :destroy]
+
+  # GET /communities/:community_id/common_expenses
   def index
+    @common_expenses = @community.common_expenses.order(date: :desc)
   end
-
-  def show
-  end
-
+  # GET /communities/:community_id/common_expenses/new
   def new
+    @common_expense = CommonExpense.new
   end
-
+  # POST /communities/:community_id/common_expenses
   def create
-  end
+    @common_expense = CommonExpense.new(common_expense_params)
+    @common_expense.community = @community
 
+    if @common_expense.save
+      redirect_to community_common_expenses_path(@community), notice: "Gasto común creado correctamente."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  #GET /common_expenses/:id
+  def show
+    @expense_details = @common_expense.expense_details
+  end
+  # GET /common_expenses/:id/edit
   def edit
   end
-
+  # PATCH/PUT /common_expenses/:id
   def update
+    if @common_expense.update(common_expense_params)
+      redirect_to common_expense_path(@common_expense), notice: "Gasto común actualizado correctamente."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  # DELETE /common_expenses/:id
+  def destroy
+    community = @common_expense.community
+    @common_expense.destroy
+    redirect_to community_common_expenses_path(community), notice: "Gasto común eliminado correctamente."
+  end
+  private
+
+  def set_community
+    @community = Community.find(params[:community_id])
   end
 
-  def destroy
+  def set_common_expense
+    @common_expense = CommonExpense.find(params[:id])
+    @community = @common_expense.community
+  end
+
+  def common_expense_params
+    params.require(:common_expense).permit(:date, :total)
   end
 end
--- app/controllers/expense_details_controller.rb
+++ app/controllers/expense_details_controller.rb
@@ -1,22 +1,65 @@
 class ExpenseDetailsController < ApplicationController
+  before_action :set_expense_detail, only: [:show, :edit, :update, :destroy]
+  before_action :set_common_expense, only: [:index, :new, :create]
+
+  # GET /common_expenses/:common_expense_id/expense_details
   def index
+    @expense_details = @common_expense.expense_details
   end
 
+  # GET /common_expenses/:common_expense_id/expense_details/new
+  def new
+    @expense_detail = ExpenseDetail.new
+  end
+
+  # POST /common_expenses/:common_expense_id/expense_details
+  def create
+    @expense_detail = ExpenseDetail.new(expense_detail_params)
+    @expense_detail.common_expense = @common_expense
+
+    if @expense_detail.save
+      redirect_to common_expense_expense_details_path(@common_expense), notice: "Detalle creado correctamente."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # GET /expense_details/:id
   def show
   end
 
-  def new
-  end
-
-  def create
-  end
-
+  # GET /expense_details/:id/edit
   def edit
   end
 
+  # PATCH/PUT /expense_details/:id
   def update
+    if @expense_detail.update(expense_detail_params)
+      redirect_to expense_detail_path(@expense_detail), notice: "Detalle actualizado correctamente."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
+  # DELETE /expense_details/:id
   def destroy
+    common_expense = @expense_detail.common_expense
+    @expense_detail.destroy
+    redirect_to common_expense_expense_details_path(common_expense), notice: "Detalle eliminado correctamente."
   end
+
+  private
+
+  def set_common_expense
+    @common_expense = CommonExpense.find(params[:common_expense_id])
+  end
+
+  def set_expense_detail
+    @expense_detail = ExpenseDetail.find(params[:id])
+  end
+
+  def expense_detail_params
+    params.require(:expense_detail).permit(:detail, :amount)
+  end
+
 end
