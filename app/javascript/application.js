565acb9aa4e32c2afa03813fd97b1bf50018cb45
Oscar Lara <oscar.lara.a@gmail.com>
2025-11-18 21:48:59 -0500

    se agrega calendario en new de booking

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,19 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+
+// Flatpickr JS
+import flatpickr from "flatpickr"
+
+document.addEventListener("turbo:load", () => {
+  flatpickr(".datetime-picker", {
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+    altInput: true,
+    altFormat: "F j, Y H:i",
+    time_24hr: true,
+    minuteIncrement: 30,
+    minDate: "today"
+  });
+});
