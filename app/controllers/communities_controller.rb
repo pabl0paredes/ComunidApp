1d1e808503f8acfadc55b1103aafdef83ab0fba2
Pablo Paredes Haz <pparedeshaz@gmail.com>
2025-11-18 22:16:54 -0300

    creado controlador y vistas sin estilos de community

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,22 +1,49 @@
 class CommunitiesController < ApplicationController
+  before_action :set_community, only: [:show, :edit, :update, :destroy]
+
   def index
+    @communities = Community.where(administrator_id: current_user.administrator.id)
   end
 
   def show
   end
 
   def new
+    @community = Community.new
   end
 
   def create
+    @community = Community.new(community_params)
+    @community.administrator_id = current_user.administrator.id
+    if @community.save
+      redirect_to communities_path()
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    @community.update(community_params)
+
+    redirect_to community_path
   end
 
   def destroy
+    @community.destroy
+
+    redirect_to communities_path, status: :see_other
+  end
+
+  private
+
+  def community_params
+    params.require(:community).permit(:name, :size, :address, :latitude, :longitude)
+  end
+
+  def set_community
+    @community = Community.find(params[:id])
   end
 end
