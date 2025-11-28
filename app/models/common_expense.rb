c25371579b0b248b09f151967ac7361c4c37f00f
gonzalo aime <aimegonzalo@gmail.com>
2025-11-28 01:03:05 +0000

    commit inicial

--- app/models/common_expense.rb
+++ app/models/common_expense.rb
@@ -2,10 +2,12 @@ class CommonExpense < ApplicationRecord
   belongs_to :community
   has_many :expense_details, dependent: :destroy
 
-  
+
 
   validates :date, presence: true
   validates :total,
             presence: true,
             numericality: { greater_than_or_equal_to: 0 }
+
+            
 end
