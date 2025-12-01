f5bc2fa1b277e0467c07cbf5e9e90f33106a8181
gonzalo aime <aimegonzalo@gmail.com>
2025-12-01 18:43:17 +0000

    front 1.0

--- app/controllers/common_expenses_controller.rb
+++ app/controllers/common_expenses_controller.rb
@@ -11,32 +11,80 @@ class CommonExpensesController < ApplicationController
         .where(community: @community)
         .order(date: :desc)
     else
-
       @common_expenses = CommonExpense
         .joins(expense_details: :expense_details_neighbors)
         .where(expense_details_neighbors: { neighbor_id: current_user.neighbor.id })
         .where(community: @community)
         .order(date: :desc)
         .distinct
+
+
+      @neighbor_amounts = {}
+
+      ExpenseDetailsNeighbor
+        .joins(:expense_detail)
+        .where(neighbor: current_user.neighbor)
+        .each do |edn|
+
+        ce = edn.expense_detail.common_expense
+        @neighbor_amounts[ce.id] ||= 0
+        @neighbor_amounts[ce.id] += edn.amount_due
+      end
+
+
+      @total_assigned = @neighbor_amounts.values.sum
+
+      @total_paid = ExpenseDetailsNeighbor
+        .where(neighbor: current_user.neighbor, status: "approved")
+        .sum(:amount_due)
+
+      @total_pending = @total_assigned - @total_paid
     end
   end
 
+
   def show
-  @common_expense = CommonExpense.find(params[:id])
-  @community = @common_expense.community
-  authorize @common_expense
+      @common_expense = CommonExpense.find(params[:id])
+      @community = @common_expense.community
+      authorize @common_expense
 
-  if current_user.neighbor
+     
+      if current_user.neighbor
 
-    @expense_details = @common_expense.expense_details
-      .joins(:expense_details_neighbors)
-      .where(expense_details_neighbors: { neighbor_id: current_user.neighbor.id })
-      .order(created_at: :desc)
-  else
 
-    @expense_details = @common_expense.expense_details.order(created_at: :desc)
+        @expense_details = @common_expense.expense_details
+          .joins(:expense_details_neighbors)
+          .where(expense_details_neighbors: { neighbor_id: current_user.neighbor.id })
+          .order(created_at: :desc)
+
+
+        neighbors_edns = ExpenseDetailsNeighbor
+          .joins(:expense_detail)
+          .where(
+            neighbor: current_user.neighbor,
+            expense_details: { common_expense_id: @common_expense.id }
+          )
+
+
+        @neighbor_total = neighbors_edns.sum(:amount_due)
+
+
+        if neighbors_edns.all?(&:approved?)
+          @neighbor_status = "approved"
+        elsif neighbors_edns.any?(&:rejected?)
+          @neighbor_status = "rejected"
+        elsif neighbors_edns.any?(&:pending?)
+          @neighbor_status = "pending_approval"
+        else
+          @neighbor_status = "unpaid"
+        end
+
+
+      else
+        @expense_details = @common_expense.expense_details.order(created_at: :desc)
+      end
   end
-end
+
 
   def new
     @common_expense = CommonExpense.new
