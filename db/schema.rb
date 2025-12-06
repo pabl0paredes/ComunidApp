5a12cae7952d8955e0d83033305010e05cb2a4fd
gonzalo aime <aimegonzalo@gmail.com>
2025-12-06 21:36:08 +0000

    estilos comon spaces

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_06_143206) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_06_155747) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "vector"
